a06abbc12ec2b4361eb4e0c36b9352ff74c58f50
Amilton Lobo <amilton.mendes@gmail.com>
2021-03-08 11:56:51 -0300

    Refatoração de código para melhor organização

--- avaliacao/avaliacao.py
+++ avaliacao/avaliacao.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import timeit
+
 class AvaliacaoAlgoritmo:
     """
     Classe para registrar o resultado das execuções de um algoritmo
     """
-
     def __init__(self,quantidade_repeticoes,lista_parametros):
         self.quantidade_repeticoes = quantidade_repeticoes
         self.lista_parametros = lista_parametros
+
     def retorna_resultados(self,funcao,nome_algoritmo):
         data=[]
         for parametro in self.lista_parametros:
@@ -15,7 +17,15 @@ class AvaliacaoAlgoritmo:
                 custo = funcao(*parametro)
                 interval =  timeit.default_timer() - starttime
                 data.append([nome_algoritmo,parametro,custo,interval])
+
         retorno = pd.DataFrame(data=data,columns=['algoritmo','parametro','custo','tempo'])
+
         return retorno
     def retorna_intervalo_confianca(self):
         pass
+
+
+
+
+
+
