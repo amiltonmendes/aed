0718c39dd13027d727372d116729b5b02d42d162
Amilton Lobo <amilton.mendes@gmail.com>
2021-03-15 16:18:13 -0300

    Melhoria da organização dos arquivos e finalização das classes utilitárias.

---
+++ estrategias/forca_bruta/brute_force_branch_no_bound.py
@@ -0,0 +1,93 @@
+import pybnb
+from estrategias.utils.utils_string_corrector_branch import OperacoesPossiveis,ListaMovimentos
+from copy import deepcopy
+import math
+
+class Simple(pybnb.Problem):
+    '''
+    Classe com a implementação dos métodos branch and bound do pacote pybnb
+    desenvolvida para adequar a heurística branch and bound ao problema
+    String to string correction SR20
+    '''
+    def __init__(self,str1,str2):
+
+        self._xL, self._xU = -len(str1)**len(str1), len(str1)**len(str1)+1
+        self.lst = ListaMovimentos(str1, str2, 3)
+        self.custo=len(str1)**len(str1)
+        self.max_operacoes = math.factorial(len(str1))
+
+    #
+    # required methods
+    #
+    def sense(self):
+        return pybnb.minimize
+    def objective(self):
+        return self.custo
+    def bound(self):
+        return self.custo-1
+    def save_state(self, node):
+        node.state = (self.lst,self._xL, self._xU, self.custo)
+    def load_state(self, node):
+        (self.lst,self._xL, self._xU, self.custo)= node.state
+    def branch(self):
+        for i in [OperacoesPossiveis.SWAP,OperacoesPossiveis.DELETE,OperacoesPossiveis.NOP,OperacoesPossiveis.BACK_POS]:
+            if (self.lst.custo_total) <= self.max_operacoes:
+                #print(len(self.lst.movimentos))
+                p = len(self.lst.movimentos)
+                lst_cp = deepcopy(self.lst)
+                if lst_cp.adiciona_movimento(i):
+                    child = pybnb.Node()
+                    if(lst_cp.is_finished()):
+                        if self.custo > lst_cp.custo_total:
+                            #self._xU = lst_cp.custo_total
+                            #self._xL = -1
+                            #self.custo = lst_cp.custo_total
+                            lista_ops = Singleton()
+                            if lista_ops.lst == None:
+                                lista_ops.lst=lst_cp
+                            elif lst_cp.custo_total<lista_ops.lst.custo_total:
+                                lista_ops.lst = lst_cp
+                    child.state = (lst_cp,self._xL,self._xU,self.custo)
+                    yield child
+            else:
+                #print('cut')
+                pass
+    def notify_solve_finished(self,
+                              comm,
+                              worker_comm,
+                              results):
+
+
+        print('Resultado: ')
+        lst = Singleton().lst
+        if lst!= None:
+            print(lst.custo_total)
+            print(lst.palavrax_modificada)
+            for i in lst.movimentos:
+                print(i.operacao.name+' ',end='')
+
+class Singleton:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.lst=None
+        return cls._instance
+
+def roda_brute(string_a,string_b):
+    sng = Singleton()
+    sng.lst = None
+    problem = Simple(string_a, string_b)
+    solver = pybnb.Solver(comm=None)
+    results = solver.solve(problem)
+    return results.objective
+
+a='ovas'
+b='avo'
+
+
+'''print(results.objective)
+print(results.wall_time)
+print(results.solution_status)'''
+
+roda_brute(a,b)
---
+++ estrategias/genetic/genetic_task.py
@@ -0,0 +1,83 @@
+from estrategias.utils.utils_string_corrector_branch import OperacoesPossiveis,ListaMovimentos
+import numpy as np
+from geneticalgorithm_modified import geneticalgorithm as ga
+import editdistance
+
+class AvaliaMovimento:
+    def __init__(self,a,b):
+        self.stringa=a
+        self.stringb=b
+    def custo(self,parametros):
+        lst = ListaMovimentos(self.stringa,self.stringb,3,True)
+        for param in parametros:
+            if param == OperacoesPossiveis.NOP.value:
+                lst.adiciona_movimento(OperacoesPossiveis.NOP)
+            elif param == OperacoesPossiveis.SWAP.value:
+                lst.adiciona_movimento(OperacoesPossiveis.SWAP)
+            elif param == OperacoesPossiveis.BACK_POS.value:
+                lst.adiciona_movimento(OperacoesPossiveis.BACK_POS)
+            elif param == OperacoesPossiveis.DELETE.value:
+                lst.adiciona_movimento(OperacoesPossiveis.DELETE)
+        #if(lst.is_same()==False):
+        #    lst.custo_total += lst.penalidade*len(lst.movimentos)
+
+        return (lst.penalidade**(2*editdistance.eval(lst.palavrax_modificada,lst.palavray))-1)\
+               +lst.custo_total
+    def palavra(self,parametros):
+        lst = ListaMovimentos(self.stringa,self.stringb,3,True)
+        for param in parametros:
+            if param == OperacoesPossiveis.NOP.value:
+                lst.adiciona_movimento(OperacoesPossiveis.NOP)
+            elif param == OperacoesPossiveis.SWAP.value:
+                lst.adiciona_movimento(OperacoesPossiveis.SWAP)
+            elif param == OperacoesPossiveis.BACK_POS.value:
+                lst.adiciona_movimento(OperacoesPossiveis.BACK_POS)
+            elif param == OperacoesPossiveis.DELETE.value:
+                lst.adiciona_movimento(OperacoesPossiveis.DELETE)
+        return lst.palavrax_modificada
+
+def distancia_genetic(a,b,k=0):
+    """
+    Função proxy para a utilização de algoritmos genéticos para o cálculo do custo
+    para a transformação de uma string em outra
+    :param a: palavra 1
+    :param b: palavra 2
+    :return: custo de transformação da string 1 em string 2 segundo o algoritmo genético
+    """
+    avalia = AvaliaMovimento(a,b)
+
+    total_populacao=3**(max(len(a),len(b)))
+    populacao_avaliada = max(total_populacao/2,10)
+    interacoes = 3000#max(populacao_avaliada,30)
+    if k==0:
+        num_parametros = len(a)
+    else:
+        num_parametros=k
+    varbound =  np.array([[0,3]]*num_parametros)
+    algorithm_param = {'max_num_iteration': interacoes,\
+                       'population_size':populacao_avaliada,\
+                       'mutation_probability':0.1,\
+                       'elit_ratio': 0.01,\
+                       'crossover_probability': 0.7,\
+                       'parents_portion': 0.1,\
+                       'crossover_type':'uniform',\
+                       'max_iteration_without_improv':100}
+    model = ga(function=avalia.custo,dimension=num_parametros,variable_type='int',variable_boundaries=varbound\
+               ,convergence_curve=False,progress_bar=False,\
+               algorithm_parameters=algorithm_param)
+    model.run()
+    return model
+a='abacs'
+b='caba'
+
+model = distancia_genetic(a,b,13)
+print(model.best_function)
+variable = model.best_variable
+#variable = [1,1,1,3,1,1,3,1,3,0,0,0,0,2]
+
+avaliacao = AvaliaMovimento(a,b)
+print(variable)
+print(avaliacao.palavra(variable))
+print(avaliacao.custo(variable))
+
+
